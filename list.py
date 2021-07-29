ee43f9b6feb29cc60ec76ef203d0357bfd67ffef
Hemalee999 <88143460+Hemalee999@users.noreply.github.com>
2021-07-29 17:31:59 +0530

    Update list.py

--- list.py
+++ list.py
@@ -6,11 +6,11 @@ for i in range(1, Number+1):
 List.sort(reverse=True)
 print("Sorting List in descending Order is = ", List)
 
-'''
+###
 OUTPUT:
 Enter the Total Number of List Elements= 3
 Enter the Value of 1 Element = 10
 Enter the Value of 2 Element = 50
 Enter the Value of 3 Element = 5
 Sorting List in descending Order is =  [50, 10, 5]
-'''
+###
