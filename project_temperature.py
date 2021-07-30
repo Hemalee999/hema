74e34d4afc1b054359cc2ff8fb4a83833821366c
Hemalee999 <88143460+Hemalee999@users.noreply.github.com>
2021-07-31 01:12:36 +0530

    Create project_temperature.py

---
+++ project_temperature.py
@@ -0,0 +1,11 @@
+CODE:-
+
+celsius = float(input('Enter temperature in Celsius: '))    
+fahrenheit = (celsius * 1.8) + 32  
+print('%0.1f  Celsius is equal to %0.1f degree Fahrenheit'%(celsius,fahrenheit)) 
+
+
+OUTPUT:-
+
+Enter temperature in Celsius: 50
+50.0  Celsius is equal to 122.0 degree Fahrenheit
